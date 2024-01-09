0ff8d92832db1d08e2d495ca0789f1a2d104f431
quachtuanhao <ghahamax7@gmail.com>
2024-01-09 23:20:44 +0700

    update

--- webbanmaytinh/src/page/modules/danhmucsanpham/nsx.php
+++ webbanmaytinh/src/page/modules/danhmucsanpham/nsx.php
@@ -10,19 +10,26 @@ if (isset($_GET['value'])) {
             <?php
             $sql = "SELECT maSanPham,tenSanPham,gia,hinhAnh from sanpham where maNSX = '$tam'";
             $result = mysqli_query($conn, $sql);
-
-            if (isset($_GET['loaiSP'])) {
+            if (isset($_GET['loaiSP']) && isset($_GET['hang']) && $_GET['loaiSP'] != 'no'  && $_GET['hang'] != 'no') {
                 $loai = $_GET['loaiSP'];
-                $sql = "SELECT maSanPham,tenSanPham,gia,hinhAnh from sanpham where maLSP = '$loai'";
+                $hang = $_GET['hang'];
+                $sql = "SELECT maSanPham,tenSanPham,gia,hinhAnh from sanpham where maLSP = '$loai' and maNSX like '$hang' ";
+                $result = mysqli_query($conn, $sql);
+            } else if (isset($_GET['loaiSP']) && $_GET['loaiSP'] != 'no') {
+                $loai = $_GET['loaiSP'];
+                $sql = "SELECT maSanPham,tenSanPham,gia,hinhAnh from sanpham where  maLSP like '$loai' ";
+                $result = mysqli_query($conn, $sql);
+            } else if (isset($_GET['hang']) && $_GET['hang'] != 'no') {
+                $hang = $_GET['hang'];
+                $sql = "SELECT maSanPham,tenSanPham,gia,hinhAnh from sanpham where maNSX like '$hang' ";
                 $result = mysqli_query($conn, $sql);
             }
-
             while ($row = mysqli_fetch_array($result)) { ?>
                 <td class="content-product-items">
                     <a class="content-product-item-wrapper" href="./index.php?action=chitietsanpham&&value=<?php echo $row['maSanPham'] ?>">
                         <img class="content-product-item img" src="./../../assets/img/<?php echo $row['hinhAnh'] ?>" alt="no">
-                        <b class="content-product-item title" style=" height: 40px"><?php echo $row['tenSanPham'] ?></b>
-                        <p class="content-product-item price"><?php echo number_format($row['gia'], $decimals = 0, $dec_point = ',', $thousand_sep = '.') . 'đ' ?></p>
+                        <b class="content-product-item title"><?php echo $row['tenSanPham'] ?></b>
+                        <p class="content-product-item price"><?php echo number_format($row['gia'], $decimals = 0, $dec_point = ',', $thousand_sep = '.') . 'Ä‘' ?></p>
                     </a>
                 </td>
             <?php
@@ -30,5 +37,4 @@ if (isset($_GET['value'])) {
             ?>
         </tr>
     </table>
-
 </div>
--- webbanmaytinh/src/page/modules/filter.php
+++ webbanmaytinh/src/page/modules/filter.php
@@ -1,13 +1,14 @@
 <?php
-include '../db/connect.php';
+include './../db/connect.php';
+
 ?>
 <div class="filter">
-    <form class="form-5" action="./index.php?action=nsx" >
-    <input type="hidden" name="action" value="nsx"></input>
-        <select class="text" name="loaiSP">
-            <option value="0" >Tìm kiếm sản phẩm theo phân loại </option>
+    <form class="form-5" action="./index.php?action=nsx">
+        <input type="hidden" name="action" value="nsx"></input>
+        <select class="text" name="loaiSP" style="height: 30px;padding:5px;margin:0 0 20px 20px;cursor: pointer;border:1px solid #ccc;border-radius:4px">
+            <option value="no">Loại sản phẩm </option>
             <?php
-            $sql = "SELECT maLoaiSanPham as id, tenLoaiSanPham as ten from loaisanpham";
+            $sql = "SELECT maLoaiSanPham as id,tenLoaiSanPham as ten from loaisanpham";
             $result = mysqli_query($conn, $sql);
             while ($row = mysqli_fetch_array($result)) {
             ?>
@@ -16,8 +17,18 @@ include '../db/connect.php';
             }
             ?>
         </select>
-
-        <input type="submit" name="submit" value="Tìm">
-
+        <select class="text" name="hang" style="height: 30px;padding:5px;margin:0 0 20px 10px;cursor: pointer;border:1px solid #ccc;border-radius:4px">
+            <option value="no">Hãng sản xuất </option>
+            <?php
+            $sql = "SELECT maNhaSanXuat as id,tenNhaSanXuat as ten from nhasanxuat";
+            $result = mysqli_query($conn, $sql);
+            while ($row = mysqli_fetch_array($result)) {
+            ?>
+                <option value="<?php echo $row['id'] ?>"><?php echo $row['ten'] ?></option>
+            <?php
+            }
+            ?>
+        </select>
+        <input type="submit" value="Lọc" style="cursor: pointer;height: 30px;padding:5px;margin-left:10px">
     </form>
 </div>
