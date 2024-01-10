d35f09bd8096fd4484bd919d3fb9a209ce2b7c25
quachtuanhao <ghahamax7@gmail.com>
2024-01-10 10:33:07 +0700

    update

--- webbanmaytinh/src/page/modules/danhmucsanpham/nsx.php
+++ webbanmaytinh/src/page/modules/danhmucsanpham/nsx.php
@@ -29,7 +29,7 @@ if (isset($_GET['value'])) {
                     <a class="content-product-item-wrapper" href="./index.php?action=chitietsanpham&&value=<?php echo $row['maSanPham'] ?>">
                         <img class="content-product-item img" src="./../../assets/img/<?php echo $row['hinhAnh'] ?>" alt="no">
                         <b class="content-product-item title"><?php echo $row['tenSanPham'] ?></b>
-                        <p class="content-product-item price"><?php echo number_format($row['gia'], $decimals = 0, $dec_point = ',', $thousand_sep = '.') . 'Ä‘' ?></p>
+                        <p class="content-product-item price"><?php echo number_format($row['gia'], $decimals = 0, $dec_point = ',', $thousand_sep = '.') . 'đ' ?></p>
                     </a>
                 </td>
             <?php
