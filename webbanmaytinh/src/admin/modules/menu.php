f89477787fc03df567f378832948bfa5a466b73e
quachtuanhao <ghahamax7@gmail.com>
2024-01-15 15:24:09 +0700

    update

--- webbanmaytinh/src/admin/modules/menu.php
+++ webbanmaytinh/src/admin/modules/menu.php
@@ -1,6 +1,7 @@
 <div class="menu">
     <ul class="menu-admin-list">
         <li class="menu-admin-item"><a class="menu-admin-item-title" href="index.php?action=quanlynhasanxuat&&query=no">Quản lý nhà sản xuất</a></li>
+        <li class="menu-admin-item"><a class="menu-admin-item-title" href="index.php?action=quanlyloaisanpham&&query=no">Quản lý loại sản phẩm</a></li>
         <li class="menu-admin-item"><a class="menu-admin-item-title" href="index.php?action=quanlysanpham&&query=no">Quản lý sản phẩm</a></li>
         <li class="menu-admin-item"><a class="menu-admin-item-title" href="index.php?action=quanlytaikhoan&&query=no">Quản lý tài khoản</a></li>
         <li class="menu-admin-item"><a class="menu-admin-item-title" href="index.php?action=quanlydonhang&&query=no">Quản lý đơn hàng</a></li>
--- webbanmaytinh/src/page/modules/khuyenmai/hienthi.php
+++ webbanmaytinh/src/page/modules/khuyenmai/hienthi.php
@@ -13,17 +13,24 @@ include '../db/connect.php';
         <td class="cart-item width150"><b>Giá trị khuyến mãi</b></td>
     </tr>
     <?php
-    $sql = "SELECT maKhuyenMai,tenKhuyenMai,ngayBatDau,ngayKetThuc,giaTriKhuyenMai
-        from khuyenmai";
+    $sql = "SELECT maKhuyenMai,tenKhuyenMai,ngayBatDau,ngayKetThuc,giaTriKhuyenMai,maLKM
+        from khuyenmai where DATEDIFF(ngayBatDau,CURDATE()) >= 0 ORDER BY ngayBatDau DESC";
     $result = mysqli_query($conn, $sql);
     while ($row = mysqli_fetch_array($result)) {
     ?>
         <tr class="cart-list">
             <td class="cart-item width150"><?php echo $row['maKhuyenMai'] ?></td>
             <td class="cart-item width150"><?php echo $row['tenKhuyenMai'] ?></td>
-            <td class="cart-item width100"><?php echo $row['ngayBatDau'] ?></td>
-            <td class="cart-item width100"><?php echo $row['ngayKetThuc'] ?></td>
-            <td class="cart-item width150"><?php echo $row['giaTriKhuyenMai'] ?></td>
+            <td class="cart-item width100"><?php echo date("d/m/Y", strtotime($row['ngayBatDau'])) ?></td>
+            <td class="cart-item width100"><?php echo date("d/m/Y", strtotime($row['ngayKetThuc'])) ?></td>
+            <td class="cart-item width150">
+                <?php if ($row['maLKM'] == "KM1") {
+                    echo number_format($row['giaTriKhuyenMai'], $decimals = 0, $dec_point = ',', $thousand_sep = '.') . 'đ';
+                } else {
+                    echo $row['giaTriKhuyenMai'] . '%';
+                }
+                ?>
+            </td>
         </tr>
 
     <?php
