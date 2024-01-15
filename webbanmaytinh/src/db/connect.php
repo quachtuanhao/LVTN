71b16eebccfdaf665cfc88c067208069b30a36a6
Phat <ntpbot1@gmail.com>
2024-01-15 15:19:22 +0700

    abc

--- webbanmaytinh/src/db/connect.php
+++ webbanmaytinh/src/db/connect.php
@@ -1,8 +1,7 @@
-<?php 
-    $server="localhost";
-    $username="root";
-    $password="";
-    $database="dbbanmaytinh";
-    $conn = mysqli_connect($server,$username,$password,$database);
-    mysqli_set_charset($conn, 'UTF8');
-?>
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "db_lvtn_hao";
+$conn = mysqli_connect($server, $username, $password, $database);
+mysqli_set_charset($conn, 'UTF8');
