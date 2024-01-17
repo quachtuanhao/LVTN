b68e79615a29c434343ae7915e5b5a88be372874
Phat <ntpbot1@gmail.com>
2024-01-17 19:48:27 +0700

    display promotion in page detail order

--- webbanmaytinh/src/page/modules/donhang/chitiet.php
+++ webbanmaytinh/src/page/modules/donhang/chitiet.php
@@ -5,11 +5,13 @@ $total = 0;
 if (isset($_GET["id"])) {
     $id = $_GET["id"];
 }
-$sql2 = "SELECT khuyenmai.maLKM as maLKM,khuyenmai.giaTriKhuyenMai as gtKM  from khuyenmai join dondathang on dondathang.maKM=khuyenmai.maKhuyenMai where dondathang.maDonDatHang=$id";
+$sql2 = "SELECT khuyenmai.maLKM as maLKM,khuyenmai.giaTriKhuyenMai as gtKM,maKhuyenMai,tenKhuyenMai  from khuyenmai join dondathang on dondathang.maKM=khuyenmai.maKhuyenMai where dondathang.maDonDatHang=$id";
 $result2 = mysqli_query($conn, $sql2);
 while ($row2 = mysqli_fetch_array($result2)) {
     $maLKM = $row2['maLKM'];
     $gtKM = $row2['gtKM'];
+    $tenKM = $row2['tenKhuyenMai'];
+    $maKM = $row2['maKhuyenMai'];
 }
 ?>
 <div class="content-header">
@@ -20,7 +22,7 @@ while ($row2 = mysqli_fetch_array($result2)) {
         <td class="cart-item width100"> <b>Sản phẩm</b></td>
         <td class="cart-item width200"> <b>Tên sản phẩm</b></td>
         <td class="cart-item width100"> <b>Đơn giá</b></td>
-        <td class="cart-item width100"> <b>Số lượng</b></td>
+        <td class="cart-item width150"> <b>Số lượng</b></td>
         <td class="cart-item width200"> <b>Số tiền</b></td>
 
     </tr>
@@ -40,23 +42,33 @@ while ($row2 = mysqli_fetch_array($result2)) {
             </td>
             <td class="cart-item width200"><?php echo $row['tenSP'] ?></td>
             <td class="cart-item width100"><?php echo number_format($row['giaSP'], $decimals = 0, $dec_point = ',', $thousand_sep = '.') . 'đ' ?></td>
-            <td class="cart-item width100"><?php echo $row['soLuong'] ?></td>
-            <td class="cart-item width200"><?php echo number_format($row['tongTien'], $decimals = 0, $dec_point = ',', $thousand_sep = '.') . 'đ' ?></td>
+            <td class="cart-item width150"><?php echo $row['soLuong'] ?></td>
+            <td class="cart-item width200"><?php echo '  ' . number_format($row['tongTien'], $decimals = 0, $dec_point = ',', $thousand_sep = '.') . 'đ' ?></td>
         </tr>
     <?php
     }
-    mysqli_close($conn);
+    ?>
+    <?php if ($maKM != 'null') { ?>
+        <tr class="cart-list">
+            <td class="cart-item width100"> Khuyến mãi</td>
+            <td class="cart-item width200"> <?php echo ($tenKM != 'null' ? $tenKM : '')  ?></td>
+            <td class="cart-item width100"> <?php echo ($maKM != 'null' ? 'Mã: ' . $maKM : '')  ?></td>
+            <td class="cart-item width150"> Giảm: <?php echo ($maLKM == 'KM1' ? number_format($gtKM, $decimals = 0, $dec_point = ',', $thousand_sep = '.') . 'đ' : $gtKM . '%')  ?></td>
+            <td class="cart-item width200"> <?php echo ($maLKM == 'KM1' ? '-' . number_format($gtKM, $decimals = 0, $dec_point = ',', $thousand_sep = '.') . 'đ' : '-' . number_format($total * $gtKM / 100, $decimals = 0, $dec_point = ',', $thousand_sep = '.') . 'đ')  ?></td>
+        </tr>
+    <?php
+    }
     ?>
 </table>
 <?php
 
 ?>
-<p>Tổng cộng : <?php
-                if ($maLKM == "KM1") {
-                    $total -= $gtKM;
-                } else if ($maLKM == "KM2") {
-                    $total -= $total * $gtKM / 100;
-                }
-                echo number_format($total, $decimals = 0, $dec_point = ',', $thousand_sep = '.') . 'đ';
-                ?>
+<p style=" float: right;width:280px">Tổng cộng : <?php
+                                                    if ($maLKM == "KM1") {
+                                                        $total -= $gtKM;
+                                                    } else if ($maLKM == "KM2") {
+                                                        $total -= $total * $gtKM / 100;
+                                                    }
+                                                    echo number_format($total, $decimals = 0, $dec_point = ',', $thousand_sep = '.') . 'đ';
+                                                    ?>
 </p>
